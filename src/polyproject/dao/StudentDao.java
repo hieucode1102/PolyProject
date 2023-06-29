a53ae5bd33bdc24c41dfeac1db3bf491ae0425d0
Hieucode <Hieucode@DESKTOP-4U1DP5K>
2023-06-29 08:03:49 +0700

    Add StudentDao class

---
+++ src/polyproject/dao/StudentDao.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package polyproject.dao;
+
+/**
+ *
+ * @author Hieucode
+ */
+public class StudentDao {
+    public void insert() {
+        
+    }
+}
